66cef4f9602d965b7d117a7e40c76af9aa0bc25b
isneru <diogoclanogueira@gmail.com>
2022-07-24 22:40:22 +0100

    project done

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import linksdata from "./data.json";
+import "./App.css";
+
+export const App = () => {
+  const [search, setSearch] = useState("");
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(event) => {
+          setSearch(event.target.value);
+        }}
+      />
+      <div className="container">
+        {linksdata
+          .filter((val) => {
+            if (search == "") {
+              return val;
+            } else if (
+              val.title.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+            ) {
+              return val;
+            } /* else if (
+            val.tags.language
+              .toLocaleLowerCase()
+              .includes(search.toLocaleLowerCase())
+          ) {
+            return val;
+          } else if (
+            val.tags.type
+              .toLocaleLowerCase()
+              .includes(search.toLocaleLowerCase())
+          ) {
+            return val;
+          } */
+          })
+          .map((value, id) => {
+            return (
+              <a
+                href={value.link}
+                rel="noreferrer"
+                target="_blank"
+                className="helplink"
+                key={id}>
+                <h1 className="title">{value.title}</h1>
+                <span className="description">{value.description}</span>
+                <span className="lang">
+                  {value.tags.language + " - " + value.tags.type.toUpperCase()}
+                </span>
+              </a>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
